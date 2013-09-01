5c165b30b952e4a19881e0a0684ae16bdccf49f4
perillaroc <perillaroc@gmail.com>
2013-09-01 21:09:44 +0800

    add online callback url

--- myapp/fileconfig.py
+++ myapp/fileconfig.py
@@ -19,4 +19,10 @@ SQLALCHEMY_DATABASE_URI="mysql+mysqldb://"+sae.const.MYSQL_USER+":"+sae.const.MY
 
 APP_KEY = '130967181'
 APP_SECRET = '7ecd6b730bfeee87ae5a4f701deeaae4'
-CALLBACK_URL =  'http://127.0.0.1:5000/api/user/auth-callback'
+if 'SERVER_SOFTWARE' in os.environ:
+    # SAE
+    CALLBACK_URL =  'http://127.0.0.1:5000/api/user/auth-callback'
+    pass
+else:
+    # Local
+    CALLBACK_URL =  'http://analytics.sinaapp.com/api/user/auth-callback'
