b24fcbad2d773b180bf24c87107fbfe592d0c3e8
perillaroc <perillaroc@gmail.com>
2013-09-14 19:06:54 +0800

    add model : WeiboList

--- myapp/api_app/api_user_view.py
+++ myapp/api_app/api_user_view.py
@@ -3,6 +3,7 @@ from flask import request, url_for, render_template, jsonify, redirect, flash
 from flask.ext.security import login_required, current_user, login_user, logout_user
 import json
 import random
+import datetime
 
 from myapp.models import User
 from myapp import app, db
@@ -15,8 +16,8 @@ else:
 from myapp.thirdparty import flickr as flickr
 
 from weibo import APIClient
-client = APIClient(app_key=app.config['APP_KEY'], \
-        app_secret=app.config['APP_SECRET'], redirect_uri=app.config['CALLBACK_URL'])
+client = APIClient(app_key = app.config['APP_KEY'], \
+        app_secret = app.config['APP_SECRET'], redirect_uri = app.config['CALLBACK_URL'])
 
 @api_app.route('/update-front-image')
 def updateFrontImage():
@@ -48,7 +49,7 @@ def updateFrontImage():
     mc = pylibmc.Client()
     mc.set("front_image_list", front_image_list)
     front_image_list_2 = mc.get("front_image_list")
-    return jsonify(front_image_list=front_image_list_2)
+    return jsonify(front_image_list = front_image_list_2)
 
 @api_app.route("/create-database")
 def createDatabase():
@@ -60,13 +61,15 @@ def loadOrCreatorUser(token):
     '''根据token载入或创建用户
     '''
 
-    user = User.query.filter_by(uid=int(token.uid)).first()
+    user = User.query.filter_by(uid = int(token.uid)).first()
     if user is None:
         print 'user is not created'
         # create user
-        user_info = client.users.show.get(uid=token.uid)
+        user_info = client.users.show.get(uid = token.uid)
         user = User(token.uid, json.dumps(user_info), json.dumps(token))
         user.active = True
+        user.create_time = datetime.datetime.now().strftime("%Y-%m-%d %H:%M:%S")
+        user.last_login_time = user.create_time
         db.session.add(user)
         db.session.commit()
         login_user(user)
@@ -75,9 +78,10 @@ def loadOrCreatorUser(token):
         # load user
         user.active = True
         user.token = token
+        user.last_login_time = datetime.datetime.now().strftime("%Y-%m-%d %H:%M:%S")
         db.session.commit()
         login_user(user)
-    
+
 
 @api_app.route('/user/auth-callback')
 def authCallback():
@@ -85,11 +89,11 @@ def authCallback():
     检查用户是否存在，不存在则创建，并载入用户
     还需要错误检查
     '''
-    authorization_code = request.args.get('code','')
+    authorization_code = request.args.get('code', '')
     token = client.request_access_token(authorization_code)
-    access_token = token.access_token # 新浪返回的token，类似abc123xyz456
-    expires_in = token.expires_in # token过期的UNIX时间
-    uid = token.uid # 用户的uid
+    access_token = token.access_token  # 新浪返回的token，类似abc123xyz456
+    expires_in = token.expires_in  # token过期的UNIX时间
+    uid = token.uid  # 用户的uid
     client.set_access_token(access_token, expires_in)
     print token
     loadOrCreatorUser(token)
--- myapp/models.py
+++ myapp/models.py
@@ -14,25 +14,27 @@ roles_users = db.Table('roles_users',
 
 
 class Role(db.Model, RoleMixin):
-    id = db.Column(db.Integer(), primary_key=True)
-    name = db.Column(db.String(80), unique=True)
+    id = db.Column(db.Integer(), primary_key = True)
+    name = db.Column(db.String(80), unique = True)
     description = db.Column(db.String(255))
 
 
 class User(db.Model, UserMixin):
-    id = db.Column(db.BigInteger(64), Sequence('id_seq'), primary_key=True)
-    email = db.Column(db.String(255), unique=True)
+    id = db.Column(db.BigInteger(64), Sequence('id_seq'), primary_key = True)
+    email = db.Column(db.String(255), unique = True)
     password = db.Column(db.String(255))
     active = db.Column(db.Boolean())
     confirmed_at = db.Column(db.DateTime())
 
-    uid = db.Column(db.BigInteger(64), unique=True)
+    uid = db.Column(db.BigInteger(64), unique = True)
     info = db.Column(db.Text())
-    update_time = db.Column(db.DateTime())
     create_time = db.Column(db.DateTime())
+    last_login_time = db.Column(db.DateTime())
+    update_time = db.Column(db.DateTime())
+
     token = db.Column(db.Text())
-    roles = db.relationship('Role', secondary=roles_users,
-                            backref=db.backref('users', lazy='dynamic'))
+    roles = db.relationship('Role', secondary = roles_users,
+                            backref = db.backref('users', lazy = 'dynamic'))
 
     def __init__(self, uid, info, token):
         self.uid = uid
@@ -41,3 +43,38 @@ class User(db.Model, UserMixin):
 
     def __repr__(self):
         return '<User %r>' % self.uid
+
+class WeiboList(db.Model):
+    __tablename__ = 'weibo_list'
+    id = db.Column(db.BigInteger(64), Sequence('id_seq'), primary_key = True)
+    uid = db.Column(db.BigInteger(64), unique = True)
+
+    user_uid = db.Column(db.BigInteger(64))
+    create_time = db.Column(db.DateTime())
+    update_time = db.Column(db.DateTime())
+    status_type = db.Column(db.Integer())
+
+    status_id = db.Column(db.BigInteger(64), unique = True)
+    create_at = db.Column(db.DateTime())
+    source = db.Column(db.Text())
+
+    original_pic = db.Column(db.Text())
+    bmiddle_pic = db.Column(db.Text())
+    thumbnail_pic = db.Column(db.Text())
+
+    geo = db.Column(db.Text())
+
+    retweeted_status = db.Column(db.Text())
+
+    reposts_count = db.Column(db.Integer())
+    comments_count = db.Column(db.Integer())
+    attitudes_count = db.Column(db.Integer())
+    visible_type = db.Column(db.Integer())
+
+    pic_urls = db.Column(db.Text())
+
+    def __init__(self):
+        pass
+
+    def __repr__(self):
+        return '<WeiboList %r>' % self.uid
