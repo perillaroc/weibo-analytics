db098102775036db6028fac41509ed9a3e375e7c
perillaroc <perillaroc@gmail.com>
2014-03-04 23:48:37 +0800

    add bootstrap-datepicker.js

--- static/js/statistic.js
+++ static/js/statistic.js
@@ -1,9 +1,18 @@
 
-$('.side_bar').affix({
-    offset: {
-      top: 100
-    , bottom: function () {
-        return (this.bottom = $('.footer').outerHeight(true))
-      }
-    }
-  })
+$(document).ready(function(){
+    $('.side_bar').affix({
+        offset: {
+          top: 100
+        , bottom: function () {
+            return (this.bottom = $('.footer').outerHeight(true))
+          }
+        }
+    });
+
+    $('.input-daterange').datepicker({
+        format: "yyyy年mm月dd日",
+        endDate: "2014/03/04",
+        language: "zh-CN",
+        autoclose: true
+    });
+});
