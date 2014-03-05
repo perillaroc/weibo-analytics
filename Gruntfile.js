42780623c123d272aa80f7b93e56edc738e217da
perillaroc <perillaroc@gmail.com>
2014-03-05 11:07:24 +0800

    Use grunt to copy files

---
+++ Gruntfile.js
@@ -0,0 +1,25 @@
+module.exports = function(grunt){
+    grunt.initConfig({
+        pkg: grunt.file.readJSON('package.json'),
+        copy:{
+            main:{
+                files:[
+                    {
+                        expand: true,
+                        cwd: 'bower_components/bootstrap/dist/',
+                        src: ['**'],
+                        dest:'static/'
+                    },
+                    {
+                        expand: true,
+                        cwd: 'bower_components/bootstrap-datepicker/',
+                        src: ['css/**','js/*','js/locals/bootstrap-datepicker.zh-CN.js'],
+                        dest:'static/'
+                    }
+                ]
+            }
+        }
+    });
+    grunt.loadNpmTasks('grunt-contrib-copy');
+    grunt.registerTask('default', ['copy']);
+}
