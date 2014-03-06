f04ced1ca0e00b825738d91877a791c306573cae
perillaroc <perillaroc@gmail.com>
2014-03-06 22:27:29 +0800

    Add month counting

--- myapp/api_app/api_statistic_view.py
+++ myapp/api_app/api_statistic_view.py
@@ -1,15 +1,15 @@
 # encoding: utf-8
 import json
 import datetime
-import string
+import calendar
 
 from flask import request, g, jsonify
 
 from flask.ext.security import login_required
 from flask.ext.sqlalchemy import SQLAlchemy
-from sqlalchemy import func
+from sqlalchemy import func, distinct
 
-from myapp.models import User, WeiboList,Calendar
+from myapp.models import User, WeiboList, Calendar
 from myapp import app, db
 from myapp.api_app import api_app
 
@@ -40,32 +40,77 @@ def get_status_count():
     """
     default_end_date = datetime.date.today()
     default_start_date = default_end_date - datetime.timedelta(days=30)
-    start_date = request.args.get('start_date', default_start_date.strftime("%Y-%m-%d"))
-    end_date = request.args.get('end_date', default_end_date.strftime("%Y-%m-%d"))
+
+    start_date = request.args.get('start_date', '')
+    if start_date == '':
+        start_date = default_start_date
+    else:
+        start_datetime = datetime.datetime.strptime(start_date, "%Y-%m-%d")
+        start_date = start_datetime.date()
+
+    end_date = request.args.get('end_date', '')
+    if end_date == '':
+        end_date = default_end_date
+    else:
+        end_datetime = datetime.datetime.strptime(end_date, "%Y-%m-%d")
+        end_date = end_datetime.date()
+
     time_interval = request.args.get('time_interval', 'day')
 
-    date_list_query = db.session.query(Calendar.date.label("date")).\
-        filter(Calendar.date >= start_date).\
-        filter(Calendar.date <= end_date).\
-        subquery()
-
-    list_by_day = db.session.query(date_list_query.c.date, func.count(WeiboList.id).label("counts")).\
-        outerjoin(WeiboList, date_list_query.c.date == func.DATE(WeiboList.created_at)).\
-        group_by(date_list_query.c.date).\
-        order_by(date_list_query.c.date).all()
-
     record_list = []
-    for one_record in list_by_day:
-        record_list.append({
-            "date": one_record[0].strftime("%Y-%m-%d"),
-            "count": one_record[1]
-        })
 
-    result = {
-        "start_date": start_date,
-        "end_date": end_date,
-        "time_interval": time_interval,
-        "record":record_list
-    }
+    if time_interval == "day":
+        date_list_query = db.session.query(Calendar.date.label("date")). \
+            filter(Calendar.date >= start_date.isoformat()). \
+            filter(Calendar.date <= end_date.isoformat()). \
+            subquery()
+
+        list_by_day = db.session.query(date_list_query.c.date, func.count(WeiboList.id).label("counts")). \
+            outerjoin(WeiboList, date_list_query.c.date == func.DATE(WeiboList.created_at)). \
+            group_by(date_list_query.c.date). \
+            order_by(date_list_query.c.date).all()
+
+        for one_record in list_by_day:
+            record_list.append({
+                "date": one_record[0].isoformat(),
+                "count": one_record[1]
+            })
+
+        result = {
+            "start_date": start_date.isoformat(),
+            "end_date": end_date.isoformat(),
+            "time_interval": time_interval,
+            "record": record_list
+        }
+
+    elif time_interval == "month":
+        first_day_of_start_month = datetime.date(start_date.year, start_date.month, 1)
+        first_day_of_end_month = datetime.date(end_date.year, end_date.month, 1)
+        first_day_of_next_end_month = first_day_of_end_month + \
+                                      datetime.timedelta(days=calendar.monthrange(end_date.year, end_date.month)[1])
+        date_list_query = db.session.query(distinct(func.DATE_FORMAT(Calendar.date, "%Y-%m")).label("d")). \
+            filter(Calendar.date >= first_day_of_start_month). \
+            filter(Calendar.date < first_day_of_next_end_month). \
+            subquery()
+
+        list_by_day = db.session.query(date_list_query.c.d, func.count(WeiboList.id).label("counts")). \
+            outerjoin(WeiboList, date_list_query.c.d == func.DATE_FORMAT(WeiboList.created_at, "%Y-%m")). \
+            group_by(date_list_query.c.d). \
+            order_by(date_list_query.c.d).all()
+
+        for one_record in list_by_day:
+            record_list.append({
+                "date": one_record[0],
+                "count": one_record[1]
+            })
+
+        result = {
+            "start_date": start_date.isoformat(),
+            "end_date": end_date.isoformat(),
+            "time_interval": time_interval,
+            "record": record_list
+        }
+    else:
+        result = {}
 
     return jsonify(result)
