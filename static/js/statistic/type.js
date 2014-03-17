e194e499336461c0c8fbf88b2b32eaa44939205e
perillaroc <perillaroc@gmail.com>
2014-03-17 22:41:35 +0800

    Try to add a tooltip, but failed.

--- static/js/statistic/type.js
+++ static/js/statistic/type.js
@@ -99,13 +99,17 @@ $(document).ready(function(){
             });
 
             $("<div class=\"row summary_container\"></div>")
-                .append("<div class=\"col-md-2\"><p>全部</p><p>"+data.total_count+"</p></div>")
+                .append("<div class=\"col-md-2\">" +
+                            "<p><a href=\"#\" class=\"item_tooltip\" title data-toggle=\"tooltip\" data-original-tittle=\"所有微博\">" +
+                                "全部</a></p>" +
+                            "<p>"+data.total_count+"</p>" +
+                        "</div>")
                 .append("<div class=\"col-md-2\"><p>原创</p><p>"+ (data.total_count-data.retweeted_count)+"</p></div>")
                 .append("<div class=\"col-md-2\"><p>转发</p><p>"+data.retweeted_count+"</p></div>")
                 .append("<div class=\"col-md-2\"><p>图片</p><p>"+data.pic_count+"</p></div>")
                 .append("<div class=\"col-md-2\"><p>位置</p><p>"+data.geo_count+"</p></div>")
-                .appendTo('#main_chart_container');
-
+                .appendTo('#main_chart_container')
+                .children('.item_tooltip').tooltip();
         });
     }
 
